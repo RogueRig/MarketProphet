0c4b44b58e61ecdaf8cc44528637325033c7b5ab
RogueRig <58204452+RogueRig@users.noreply.github.com>
2026-01-09 16:39:55 +0530

    Update build.ts

--- script/build.ts
+++ script/build.ts
@@ -36,7 +36,12 @@ async function buildAll() {
   await rm("dist", { recursive: true, force: true });
 
   console.log("building client...");
-  await viteBuild();
+  await viteBuild({
+  build: {
+    outDir: "public",
+    emptyOutDir: true,
+  },
+});
 
   console.log("building server...");
   const pkg = JSON.parse(await readFile("package.json", "utf-8"));
